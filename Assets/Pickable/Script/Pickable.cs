ff4e7afbc8a49e8ca8355cff800173573070c679
fajarfh <roseorchiddawn@gmail.com>
2025-09-06 19:59:56 +0700

    Materi 4
    Materi 4 : Pickable

---
+++ Assets/Pickable/Script/Pickable.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    [SerializeField]
+    public PickableType pickableType;
+
+    public Action<Pickable> OnPicked; // Fix CS1593: Use Action<Pickable> to accept one argument
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("PIckeup : " + pickableType);
+            Destroy(gameObject);
+
+            OnPicked?.Invoke(this); // Fix IDE1005: Use null-conditional and Invoke
+        }
+    }
+
+}
